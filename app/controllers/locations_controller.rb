e02164a7c3d223a4fa26fd3455dab1ddd904dccc
Kurnia Muhamad <omenkzz@gmail.com>
2019-10-02 03:52:30 +0700

    Remove locations menu

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,8 +6,6 @@ class LocationsController < ApplicationController
       @locations = Location.near(params[:search], 50, :order => :distance)
       @center = Geocoder.search(params[:search]).first.coordinates.reverse
       @geojson = build_geojson
-    else
-      @locations = Location.all
     end
   end
 
